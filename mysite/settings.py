6b22d4e6cc65b1f36e97f39b87ced7089214a4d5
PeterTZN <jeep11@hotmail.com>
2026-05-27 18:16:21 +0100

    COB commit.

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resturant_menu',
+    'restaurant_menu',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -15,11 +15,11 @@ STATUS = (
 )
 
 
-class Items(models.Model):
+class Item(models.Model):
     meal = models.CharField(max_length=1000, unique=True)
     description = models.CharField(max_length=2000)
-    price = models.DecimalField(decimal_places=2)
-    meal_type = models.CharField(choices=MEAL_TYPE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     status = models.IntegerField(choices=STATUS, default=0)
     date_created = models.DateTimeField(auto_now_add=True)
